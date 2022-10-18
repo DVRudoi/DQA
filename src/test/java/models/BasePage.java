0e43b75efe61269f68a672f24e8ecc31f897fd04
DVRudoi <dvrudoy@gmail.com>
2022-10-19 00:38:24 +0300

    BasePage, BaseTest

---
+++ src/test/java/models/BasePage.java
@@ -0,0 +1,23 @@
+package models;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class BasePage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public WebDriverWait getWait10() {
+        if (wait == null){
+            wait = new WebDriverWait(getDriver(),Duration.ofSeconds(10));
+        }
+
+        return wait;
+    }
+}
