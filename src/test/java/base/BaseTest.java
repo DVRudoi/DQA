aca9a9be88a4b4c78f24c23721cda3f43fbe9d09
DVRudoi <dvrudoy@gmail.com>
2022-10-21 00:58:06 +0300

    Add ChromeOptions(window size and position)

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,8 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -11,7 +13,7 @@ import java.time.Duration;
 
 public abstract class BaseTest {
     private WebDriver driver;
-
+    private ChromeOptions options;
     protected WebDriver getDriver() {
         return driver;
     }
@@ -23,19 +25,22 @@ public abstract class BaseTest {
     @BeforeClass
     protected void before () {
         WebDriverManager.chromedriver().setup();
+        options = new ChromeOptions();
+        options.addArguments("--window-size=1360,768");
+        options.addArguments("--window-position=0,0");
     }
 
     @BeforeMethod
     protected void beforeTest(){
-        driver = new ChromeDriver();
-        getDriver().manage().window().maximize();
+        driver = new ChromeDriver(options);
+//        getDriver().manage().window().maximize(); //if need to maximize window;
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         getWeb();
     }
 
-//    @AfterMethod
-//    protected void afterTest() {
-//        getDriver().quit();
-//    }
+    @AfterMethod
+    protected void afterTest() {
+        getDriver().quit();
+    }
 
 }
